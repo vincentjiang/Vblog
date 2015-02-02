7dbb9bb622e64a57a7daf594425a01bfd2d4c886
VincentJiang <jxs471494539@gmail.com>
2015-02-02 23:29:30 +0800

    update meta information.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def page_title
     @page_title || ENV["BLOG_NAME"]
   end
+
+  def meta_description
+    @meta_description || "VincentJiang's Blog"
+  end
+
+  def keyword
+    @keyword = "#{@post.title if @post} ruby rails RubyOnRails web develop"
+  end
+
 end
